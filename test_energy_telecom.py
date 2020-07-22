f0a3755991c987d265955d45f4fabbabbeec6bf2
DaniilVolg <59584072+DaniilVolg@users.noreply.github.com>
2020-07-23 00:17:36 +0300

    Update test_energy_telecom.py

--- test_energy_telecom.py
+++ test_energy_telecom.py
@@ -3,7 +3,7 @@ import pytest
 @pytest.fixture
 def visit_site(py):
     py.visit("http://energy-telecom.portnov.com/qa/")
-
+  #Contact_information part
 def test_contact_information(py,visit_site):
     field_name = py.getx("//input[@id='firstName']").type("Jack")
     field_last_name = py.getx("//input[@name='lastName']").type("Black")
@@ -17,3 +17,15 @@ def test_contact_information(py,visit_site):
     phone_field_3 = py.getx("//input[@id='phone3']").type("5056")
     best_way_to_contact_field = py.getx("//select[@name='bestWayToContact']").select("Phone")
     refferd_by_field = py.getx("//input[@name='refferedBy']").type("Racoon")
+
+
+
+ #Cell Phone Service field
+    current_provider_field = py.getx("//select[@name='cellPhoneCurrentProvider']").select("Verizon")
+    im_happy_with_current_provider_field = py.webdriver.find_element_by_name("cellPhoneSatisfiedWithProvider[]").click()
+    my_monthly_payment_field = py.getx("//input[@name='cellPhoneMonthlyBill']").type("150")
+    minutes_to_plan_field = py.getx("//select[@name='cellPhoneMinutesOnPlan']").select("200")
+    month_left_on_current_contract_field = py.getx("//select[@name='cellPhoneMonthsOnCurrent']").select("4")
+    numbers_of_phones_plan_field = py.getx("//select[@name='cellPhoneNumberOfPhones']").select("3")
+    last_upgraded_phone = py.getx("//select[@name='cellPhoneLastUpgradeMonths']").select("6")
+    im_interested_in_wireless_radio_button = py.webdriver.find_element_by_name("cellPhoneWirelessCard[]").click()
